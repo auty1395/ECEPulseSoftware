6bcc29a8a70745eba576933ebae6d57d6ea02b9a
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-02-14 14:41:17 -0600

    Files

--- puzzle3/pass.c
+++ puzzle3/pass.c
@@ -1,5 +1,12 @@
-int PASS(char * st_text){
+int PASS(char * st_text)
+{
+    
+    static char file[] = "database.txt";
 
+    FILE *fp = fopen(file, 'r');
+    encrypt(st_text);
+
+    while (NULL != fread(
 
 
 
