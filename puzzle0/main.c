086a21b68e5ffd3b6855fdf2a0a754028b16935b
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-02-14 11:09:44 -0600

    Puzzle 0

---
+++ puzzle0/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Name: Jeffrey Huang        NetID: jhuang61\n");
+    printf("Name: Adam Long            NetID: arlong2\n");
+    printf("Name: Adam Auten           NetID: auten2\n");
+
+    printf("\nTier 2\n");
+    return 0;
+}
