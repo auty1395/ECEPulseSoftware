759a5c2850c17a108b802e44eff7530cbd4dd261
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-02-14 14:49:17 -0600

    Files add pass

--- puzzle3/files.c
+++ puzzle3/files.c
@@ -1,1 +1,20 @@
 #include "files.h"
+
+bool add_pass_to_buf(char *str)
+{
+    static char file[] = "passwords.txt";
+
+    FILE *fp = fopen(file, 'a');
+    int len = strlen(str);
+
+    if (fp == NULL)
+    {
+        printf("Add: There was an error opening the file\n");
+        return FALSE;
+    }
+
+    fwrite(str, len, 1, fp);
+    fclose(fp);
+    return TRUE;
+
+}
--- puzzle3/puzzle3.h
+++ puzzle3/puzzle3.h
@@ -1,3 +1,6 @@
+#define TRUE 1
+#define FALSE 0
+
 int ADD(char * st_text);
 int DELETE(char * st_text);
 int PASS(char * st_text);
