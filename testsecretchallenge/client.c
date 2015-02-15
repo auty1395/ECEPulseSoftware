887e4bd9583419588d7c1a3123dc5e2d68c0e954
Adam <auty1395@gmail.com>
2015-02-14 22:07:32 -0600

    fjds

---
+++ testsecretchallenge/client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <time.h>
+
+
+int main(int argc, char **argv)
+{
+    int s;
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct addrinfo hints, *result;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET; /* IPv4 only */
+    hints.ai_socktype = SOCK_STREAM; /* TCP */
+
+    s = getaddrinfo(NULL, argv[1], &hints, &result);
+    if (s != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+            exit(1);
+    }
+
+    connect(sock_fd, result->ai_addr, result->ai_addrlen);
+		srand(time(0));
+	while(1) {
+		switch((rand()%3))
+		{
+			case 0:
+				write(sock_fd, "BLOCK", 5);
+				break;
+			case 1:
+        write(sock_fd, "SHOOT", 5);
+				break;
+			case 2:
+    	  write(sock_fd, "RELOAD", 6);
+				break;
+			default:
+				break;
+		}
+		write(sock_fd, "     ", 5); 
+	}
+
+
+    return 0;
+}
